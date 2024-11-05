94fa269fdba9ca0b42a3f9ae94e557dab0decbc2
Benjamin Tsai <coding47414@gmail.com>
2024-11-05 17:39:34 +0800

    Initial commit.

---
+++ code1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    for (int i = a; i >= 0; i--)
+        cout << "Count down: " << i << endl;
+    return 0;
+}
